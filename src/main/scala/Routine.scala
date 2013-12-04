2ef494b9fde44f630556d1c6574724de9c930781
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2013-12-04 19:19:20 +0100

    Implement swapping behaviours in stream

--- src/main/scala/Routine.scala
+++ src/main/scala/Routine.scala
@@ -1,4 +1,4 @@
-case class Behaviour(cell: Cell)
+case class Behaviour(cell: Cell, tc: TimeChunk)
 
 object RoutineGenerator {
   implicit class RandomList[T](l: List[T]) {
@@ -13,17 +13,36 @@ object RoutineGenerator {
     }
 
     val cell = (tc: TimeChunk) => {
-      for {
+      (for {
         types <- getTypes(tc)
         cells <- world if cells._2.typ == types
-      } yield cells._2
+      } yield cells._2, tc)
     }
 
     val cells = List(cell(Morning), cell(Afternoon), cell(Evening),
       cell(Night))
 
-    val behaviours = cells map ((x: List[Cell]) => Behaviour(x.shuffle.head))
+    // ogromny błąd - może wylosować np. dwa różne domy na wieczór i na noc :D
+    // TODO
+
+    val behaviours = cells map { case (x, tc) => Behaviour(x.shuffle.head, tc) }
 
     Stream.continually(behaviours).flatten
+  }
+
+  def swapFirst(b: Behaviour)(s: Stream[Behaviour]): Stream[Behaviour] = {
+    import collection.immutable.Stream.Empty
+    def mapOnce[A](f: A => Option[A], xs: Stream[A]): Stream[A] = xs match {
+      case Empty => Empty
+      case a #:: as => f(a) match {
+        case None => a #:: mapOnce(f, as)
+        case Some(s) => s #:: as
+      }
+    }
+
+    def swap(newB: Behaviour)(b: Behaviour): Option[Behaviour] =
+      if (b.tc == newB.tc) Some(newB) else None
+
+    mapOnce(swap(b)_, s)
   }
 }
