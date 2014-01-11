89c157785e50bb4a455b0fd5094f718d154b0c1a
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-01-11 20:03:20 +0100

    Implement simple way of spreading disease
    I'm not sure if the change to use adjacent cells naively
    won't result in duplicating people, so commiting this
    'safe' implementation.

--- src/main/scala/gines/simulation/SimulationState.scala
+++ src/main/scala/gines/simulation/SimulationState.scala
@@ -8,14 +8,39 @@ case class SimulationState(
   agents: List[Person],
   world: Map[(Int, Int), Cell]) {
   def step(f: Person=>Person): SimulationState = {
-    val people = agents map (f(_).nextPhase)
+    lazy val (ill, rest) = agents partition (_.health match {
+      case Ill(_) => true
+      case _ => false
+    })
+
+    lazy val (healthy, immune) = rest partition (_.health match {
+      case Healthy => true
+      case _ => false
+    })
+
+    def worldAdjacent(w: Map[Cell, (Int, Int)], c1: Cell, c2: Cell): Boolean = {
+      if (c1.typ != c2.typ)
+        false
+      else {
+        val (x1, y1) = w(c1)
+        val (x2, y2) = w(c2)
+        (x1 == x2 && math.abs(y1 - y2) == 1) || (math.abs(x1 - x2) == 1 && y1 == y2)
+      }
+    }
+
+    lazy val outsideWorld = world.map(_.swap)
+
+    lazy val healthyPeopleInSameCellAsIll = ill map (i => healthy filter (_.routine.head.cell == i.routine.head.cell)) flatten
+    lazy val potentiallyInfectedPeople = healthyPeopleInSameCellAsIll map (f(_))
+    lazy val allPeople = (potentiallyInfectedPeople ++ ill ++ immune) map (_.nextPhase)
     val nextChunk = chunk match {
       case Morning => Afternoon
       case Afternoon => Evening
       case Evening => Night
       case Night => Morning
     }
-    SimulationState(day+1, nextChunk, people, world)
+
+    SimulationState(if (nextChunk == Morning) day + 1 else day, nextChunk, allPeople, world)
   }
 }
 
